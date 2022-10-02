5bfda0b8800e4b70436cd1ef81371625680ff0c3
Shriti <shriti2018@gmail.com>
2022-10-02 23:18:37 +0700

    Day 1 of 30 days of code

---
+++ Day0.py
@@ -0,0 +1,6 @@
+#To complete this challenge, you must save a line of input from stdin to a variable,
+#print Hello, World. on a single line, and finally print the value of your variable on a second line.
+
+print('Hello,World.')
+input_string=input('30 Days of Code')
+print(input_string)
