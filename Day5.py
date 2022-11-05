8e81f84de8c2d6d190a31affaa35d37077a534c8
Shriti Singh <SHRITI2018@GMAIL.COM>
2022-11-06 01:04:57 +0530

    Working with loops

---
+++ Day5.py
@@ -0,0 +1,14 @@
+# Loops
+
+import math
+import os
+import random
+import re
+import sys
+
+print("Enter your number")
+if __name__ == '__main__':
+    n = int(input().strip())
+for i in range(1,11):
+    s = n*i
+    print(n,"x",i,"=",s)
